c4904d9b401baaa50a471c192eff8ccb6b87bea3
thesmug <148411048+letMeHaveThesmugBack@users.noreply.github.com>
2025-02-12 21:24:56 -0500

    Add the entire bot

---
+++ JSONReader.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYB
+{
+    class JSONReader
+    {
+        public string Token { get; set; }
+
+        public async Task Read()
+        {
+            using (StreamReader reader = new StreamReader("properties.json"))
+            {
+                string json = await reader.ReadToEndAsync();
+                JSONLayout properties = JsonConvert.DeserializeObject<JSONLayout>(json);
+
+                Token = properties.Token;
+            }
+        }
+    }
+
+    internal sealed class JSONLayout
+    {
+        public string Token { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using DSharpPlus;
+using System;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FYB
 {
     class Program
     {
-        static void Main(string[] args)
+        private static DiscordClient Client { get; set; }
+
+        static async Task Main(string[] args)
         {
+            JSONReader reader = new JSONReader();
+            await reader.Read();
+
+            DiscordConfiguration config = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.GuildMessages | DiscordIntents.MessageContents,
+                Token = reader.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.MessageCreated += Client_MessageCreated;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task Client_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs args)
+        {
+            if (args.Author.IsBot) return;
+
+            Match match = Regex.Match(args.Message.Content, "(.*)(g)(.*)(u)(.*)(s)(.*)");
+
+            if (!match.Success) return;
+
+            StringBuilder builder = new StringBuilder().Append("FUCK YOU! BABIES.\n```ansi\n");
+
+            for (int i = 1; i < match.Groups.Count; i++)
+            {
+                switch (match.Groups[i].ToString())
+                {
+                    case "g":
+                    case "u":
+                    case "s":
+                        builder.Append("\u001b[0;41;30m");
+                        builder.Append(match.Groups[i]);
+                        break;
+                    default:
+                        builder.Append("\u001b[0;47;30m");
+                        builder.Append(match.Groups[i]);
+                        break;
+                }
+            }
+
+            builder.Append("```");
+
+            await args.Message.RespondAsync(builder.ToString());
         }
     }
 }
